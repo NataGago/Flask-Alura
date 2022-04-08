5e8e326d77069717fdb394a2cb620bfb2a08456d
Nata Gago <natagago123@gmail.com>
2022-04-07 22:18:04 -0300

    Primeiro commit teste

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, jsonify
+
+app = Flask(__name__)
+
+class Jogo():
+    
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+    
+lista = [jogo1, jogo2, jogo3]
+
+@app.get('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.get('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.post('/criar')
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+@app.get('/login')
+def login():
+    return render_template('login.html')
+
+@app.post('/autenticar')
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+# app.run(host='0.0.0.0', port=8080, debug=True)
