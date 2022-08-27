573a801761c5c0db3624e5b55508b96c3bcc26fe
Nata Gago <natagago123@gmail.com>
2022-08-27 01:23:31 -0300

    Realizado a criação da tabela com Python e MySQL juntamente com Create do CRUD

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import (
     Flask,
     flash,
@@ -8,48 +9,52 @@ from flask import (
     flash,
     url_for,
 )
+from flask_sqlalchemy import SQLAlchemy
+import tomli
+
+# Carregando variaveis de banco de dados
+with open("config.toml", mode="rb") as fp:
+    config = tomli.load(fp)
+
+DB_SGBD = config["database"]["DB_SGBD"]
+DB_USER = config["database"]["DB_USER"]
+DB_PASSWORD = config["database"]["DB_PASSWORD"]
+DB_SERVER = config["database"]["DB_SERVER"]
+DB_PORT = config["database"]["DB_PORT"]
+DB_DATABASE = config["database"]["DB_DATABASE"]
+
 
 app = Flask(__name__)
 app.secret_key = "alura"
-# app.run(host='0.0.0.0', port=8080, debug=True)
 
-# export FLASK_APP=app.py
-# export FLASK_ENV=development
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"{DB_SGBD}://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:{DB_PORT}/{DB_DATABASE}"
 
+db = SQLAlchemy(app)
 
-class Jogo:
-    def __init__(self, nome, categoria, console) -> None:
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    nome = db.Column(db.String(50), nullable = False)
+    categoria = db.Column(db.String(40), nullable = False)
+    console = db.Column(db.String(20), nullable = False)
+    
+    def __repr__(self) -> str:
+        return '<Name %r' % self.name
 
+class Usuarios(db.Model):
+    nickname = db.Column(db.String(8), primary_key= True)
+    nome = db.Column(db.String(20), nullable = False)
+    senha = db.Column(db.String(100), nullable = False)
+    
+    def __repr__(self) -> str:
+        return '<Name %r' % self.name
 
-jogo1 = Jogo("Super Mario", "Ação", "SNES")
-jogo2 = Jogo("Pokemon Gold", "RPG", "GBA")
-jogo3 = Jogo("Mortal Kombat", "Luta", "SNES")
-lista = [jogo1, jogo2, jogo3]
-
-
-class Usuario:
-    def __init__(self, nome, nickname, senha) -> None:
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
-
-
-usuario1 = Usuario("Natã Gago", "natagago", "mestra")
-usuario2 = Usuario("Gabriel Gago", "bielgago", "210799")
-usuario3 = Usuario("Ronaldo Gago", "rogago", "gabitan")
-
-usuarios = {
-    usuario1.nickname: usuario1,
-    usuario2.nickname: usuario2,
-    usuario3.nickname: usuario3,
-}
 
 
 @app.get("/")
 def index():
+    lista = Jogos.query.order_by(Jogos.id)
     return render_template("lista.html", titulo="Jogos", jogos=lista)
 
 
@@ -65,8 +70,17 @@ def criar():
     nome = request.form["nome"]
     categoria = request.form["categoria"]
     console = request.form["console"]
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
+    
+    jogo = Jogos.query.filter_by(nome=nome).first()
+
+    if jogo:
+        flash("Jogo já existente.")
+        return redirect(url_for("index"))
+    
+    novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+
     return redirect(url_for("index"))
 
 
@@ -78,17 +92,16 @@ def login():
 
 @app.post("/autenticar")
 def autenticar():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[request.form['usuario']]
+    usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
+    if usuario:
         if request.form['senha'] == usuario.senha:
             session['usuario_logado'] = usuario.nickname
-            flash(f"{usuario.nome} logado com sucesso!")
-            proxima_pagina = request.form["proxima"]
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
             return redirect(proxima_pagina)
     else:
-        flash("Usuário não logado.")
-        return redirect(url_for("login"))
-
+        flash('Usuário não logado.')
+        return redirect(url_for('login'))
 
 @app.get("/logout")
 def logout():
